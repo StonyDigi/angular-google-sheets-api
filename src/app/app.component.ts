b086ac85fb1ce9ec452342dcf67db361c255e8e0
StonyDigi <stony86@gmail.com>
2023-06-01 21:37:01 +0200

    további  html tartalom + css kód hozzáadva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'google-sheets-api';
+
+  options = ['14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00'];
+
+  limitSelections(): void {
+    const selectedOptions = this.options.filter(option => this.isSelected(option));
+    
+    if (selectedOptions.length > 2) {
+      const lastSelectedOption = selectedOptions[selectedOptions.length - 1];
+      this.deselectOption(lastSelectedOption);
+    }
+  }
+
+  isSelected(option: string): boolean {
+    return (document.querySelector(`input[type="radio"][name="group"][value="${option}"]`) as HTMLInputElement).checked;
+  }
+
+  deselectOption(option: string): void {
+    (document.querySelector(`input[type="radio"][name="group"][value="${option}"]`) as HTMLInputElement).checked = false;
+  }
 }
+
